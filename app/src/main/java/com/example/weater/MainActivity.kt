3fa5da8e7c573a9a513ccb92752eedae70704b25
Ekaterina1112 <medngueu@gmail.com>
2021-06-21 20:08:40 +0700

    Рабочая версия

--- app/src/main/java/com/example/weater/MainActivity.kt
+++ app/src/main/java/com/example/weater/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.net.HttpURLConnection
 import java.net.URL
@@ -31,25 +33,46 @@ class MainActivity : AppCompatActivity() {
                     var key1: String = "a6b8a9bf88133f80b4f115fe3f525f4b"
                     var URLad: String =
                         "http://api.openweathermap.org/data/2.5/weather?q=" + city1 + "&appid=" + key1 + "&units=metric&lang=ru"
+                    val пetURLData = GetURLData(URLad, result)
+                    пetURLData.execute()
 
-                    GetURLData().execute(URLad)
                 }
             }
         }
         )
     }
-class GetURLData() : AsyncTask <String, String, String>(){
-     fun onPreExecute(result: TextView) {
-        super.onPreExecute()
-        result.setText("Ожидайте...")
+
+    class GetURLData(val URLad: String,  var _Result: TextView) : AsyncTask<String, String, String>() {
+
+
+//    constructor(var uRLad: String) {
+//        URLad = uRLad
+//    }
+
+        fun onPreExecute(result: TextView) {
+            super.onPreExecute()
+            result.setText("Ожидайте...")
+        }
+
+
+        override fun doInBackground(vararg params: String?): String {
+
+            var line: String = "000"
+            var connect = URL(URLad).openConnection() as HttpURLConnection
+            connect.inputStream.bufferedReader().use { reader ->
+                line = reader.readLine()
+                var jsonObject = JSONObject(line)
+                var lineMain = jsonObject.get("main") as JSONObject
+                var lineTemp = lineMain.get("temp").toString()
+//                var lineArr = lineMain.get("weather") as JSONArray
+                line = lineTemp
+                _Result.setText(line)
+
+            }
+
+            return line
+        }
+
+
     }
-
-     fun doInBackground(vararg params: String?,  URLad: String): String {
-        var connect = URL(URLad).openConnection() as HttpURLConnection
-        var reader = connect.inputStream.bufferedReader().readText()
-        var line: String = ""
-
-    }
-
-}
 }
